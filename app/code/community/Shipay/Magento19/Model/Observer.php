e7b3e99e2c03e2ac01986fdd53c3b4adb11654ec
Carlessso <41929217+Carlessso@users.noreply.github.com>
2022-04-01 16:01:57 -0300

    Update Observer.php
    fix: updating cron calls

--- app/code/community/Shipay/Magento19/Model/Observer.php
+++ app/code/community/Shipay/Magento19/Model/Observer.php
@@ -2,44 +2,192 @@
 
 class Shipay_Magento19_Model_Observer {
 
+  const URI = '/refresh-token';
+  
   /**
    * Function to refresh token
    */
   public function refreshToken() {
-    $url = Mage::getBaseUrl() . 'shipaymagento19/refreshtoken';
-  
+    $storeId = Mage::app()->getStore()->getStoreId();
+
+    $response = $this->doRefreshRequest();
+    if ($response != false) {
+      Mage::getModel('core/config')->saveConfig('payment/shipay_keys/access_token', $response['access_token'], $storeId);
+      Mage::getModel('core/config')->saveConfig('payment/shipay_keys/refresh_token', $response['refresh_token'], $storeId);
+    }
+  }
+
+  /**
+   * Function to do request
+   * @return mixed
+   */
+  public function doRefreshRequest() {
+    $refreshToken = $this->getRefreshToken();
+    if ($refreshToken == false) {
+      return false;
+    }
+    $environment = $this->getEnvironment();
+
     $curl = curl_init();
+
     curl_setopt_array($curl, array(
-      CURLOPT_URL => $url,
+      CURLOPT_URL => ($environment . self::URI),
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_ENCODING => "",
       CURLOPT_MAXREDIRS => 10,
       CURLOPT_TIMEOUT => 60,
       CURLOPT_FOLLOWLOCATION => TRUE,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET"
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_HTTPHEADER => [
+        "Authorization: Bearer $refreshToken",
+        "Content-Type: application/json"
+      ],
     ));
-    curl_exec($curl);
+
+    $response = curl_exec($curl);
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    curl_close($curl);
+
+    if ($httpCode != 200) {
+      return false;
+    }
+
+    return json_decode($response, true);
+  }
+
+  /**
+   * Function to get refresh token
+   * @return mixed
+   */
+  public function getRefreshToken() {
+    $storeId = Mage::app()->getStore()->getStoreId();
+
+    try {
+      $refreshToken = Mage::getStoreConfig('payment/shipay_keys/refresh_token', $storeId);
+      if (isset($refreshToken) && !empty($refreshToken)) {
+        return $refreshToken;
+      } else {
+        return false;
+      }
+    } catch (\Throwable $th) {
+      return false;
+    }
+  }
+
+  /**
+   * Function to get environment
+   * @return string
+   */
+  public function getEnvironment() {
+    $classGetEnvironment = new Shipay_Magento19_Resource_GetEnvironment();
+    return $classGetEnvironment->getEnvironment();
   }
 
   /**
    * Function to verify order status
    */
   public function verifyOrderStatus() {
-    $url = Mage::getBaseUrl() . 'shipaymagento19/pendingorder';
-  
+    $orderCollection = Mage::getModel('sales/order')
+      ->getCollection()
+      ->addFieldToFilter('status', ['eq' => 'pending_payment']);
+
+    foreach ($orderCollection as $order) {
+      $method = $order->getPayment()->getMethod();
+
+      if ($method == 'shipay_payments') {
+        $createdAt = $order->getCreatedAt();
+        $createdAt = $this->sumDateToCompareShipay($createdAt);
+        $currentDate = date("Y-m-d h:i:s");
+
+        if ($createdAt < $currentDate) {
+          $orderTransId = $order->getPayment()->getCcTransId();
+          $response = $this->doVerifyRequest($orderTransId);
+          $status = $this->getStatus($response);
+
+          if ($status == 'approved') {
+            $order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true);
+            $order->setStatus(Mage_Sales_Model_Order::STATE_PROCESSING, true);
+            $order->save();
+          } else if ($status == 'cancelled' || $status == 'expired') {
+            $order->setState(Mage_Sales_Model_Order::STATE_CANCELED, true);
+            $order->setStatus(Mage_Sales_Model_Order::STATE_CANCELED, true);
+            $order->cancel();
+            $order->save();
+          }
+        }
+      }
+    }
+  }
+
+   /**
+   * Function to sum 3 days in createdAt date order
+   * @param string $createdAt
+   * @return string
+   */
+  public function sumDateToCompareShipay($createdAt): string {
+    return date("Y-m-d h:i:s", strtotime('+3 days', strtotime($createdAt)));
+  }
+
+  /**
+   * Function to get status from reponse
+   * @param string $response
+   * @return string
+   */
+  public function getStatus($response) {
+    $arrayResponse = json_decode($response, true);
+    $status = $arrayResponse['status'];
+    return $status;
+  }
+
+  /**
+   * Function to do request query
+   * @param string $orderId
+   */
+  public function doVerifyRequest($orderId) {
+    $accessToken = $this->getAccessToken();
+    $environment = $this->getEnvironment();
+
     $curl = curl_init();
+
     curl_setopt_array($curl, array(
-      CURLOPT_URL => $url,
+      CURLOPT_URL => ($environment . "/order/$orderId"),
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_ENCODING => "",
       CURLOPT_MAXREDIRS => 10,
       CURLOPT_TIMEOUT => 60,
       CURLOPT_FOLLOWLOCATION => TRUE,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET"
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => [
+        "Authorization: Bearer $accessToken",
+        "Content-Type: application/json"
+      ],
     ));
-    curl_exec($curl);  
+
+    $response = curl_exec($curl);
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    curl_close($curl);
+
+    if ($httpCode > 299 || $httpCode < 200) {
+      $arrayFake = [
+        'status' => 'not found'
+      ];
+      return json_encode($arrayFake);
+    }
+
+    return $response;
+  }
+
+  /**
+   * Function to get access token
+   * @return string
+   */
+  public function getAccessToken() {
+    $classGetAccessToken = new Shipay_Magento19_Resource_GetAccessToken();
+    return $classGetAccessToken->getAccessToken();
   }
 
   /**
